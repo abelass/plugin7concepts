e2e4398b90da9103dfdfc70db915aa5b34e78dc1
abelass <websolutions@mychacra.net>
2013-03-14 15:36:43 +0100

    base fabrique

---
+++ 7concepts_pipelines.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Utilisations de pipelines par 7concepts
+ *
+ * @plugin     7concepts
+ * @copyright  2013
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\7concepts\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+
+?>
---
+++ lang/paquet-7concepts_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// 7
+	'7concepts_description' => '7concepts',
+	'7concepts_nom' => '7concepts',
+	'7concepts_slogan' => '7concepts',
+);
+
+?>
